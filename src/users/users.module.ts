64c0fa13b0278d6c906dff903f0f93af4357c321
FrontEndKP <ia04pranaavkumar@gmail.com>
2023-01-18 23:51:07 +0200

    second commit

---
+++ src/users/users.module.ts
@@ -0,0 +1,25 @@
+import {forwardRef, Module} from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import {SequelizeModule} from "@nestjs/sequelize";
+import {User} from "./users.model";
+import {Role} from "../roles/roles.model";
+import {UserRoles} from "../roles/user-roles";
+import {RolesService} from "../roles/roles.service";
+import {RolesModule} from "../roles/roles.module";
+import {AuthModule} from "../auth/auth.module";
+
+@Module({
+    controllers: [UsersController],
+    providers: [UsersService],
+    imports:[
+        SequelizeModule.forFeature([Role, User, UserRoles]),
+        RolesModule,
+        forwardRef(() => UsersModule)
+    ],
+    exports: [
+        UsersService,
+
+    ]
+})
+export class UsersModule {}

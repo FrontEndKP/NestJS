9aaa056970c76b8e2d27bd9b8963add2e66c900b
FrontEndKP <121147378+FrontEndKP@users.noreply.github.com>
2023-01-27 14:48:52 +0200

    Update users.service.js

--- dist/users/users.service.js
+++ dist/users/users.service.js
@@ -29,7 +29,7 @@ let UsersService = class UsersService {
     async createUser(dto) {
         try {
             const user = await this.userRepository.create(dto);
-            const role = await this.roleService.getRoleByValue('ADMIN');
+            const role = await this.roleService.getRoleByValue('USER');
             user.roles = [role];
             await user.$set('roles', [role.id]);
             if (user) {
